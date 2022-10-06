4c317dbf717eadb413a243fa22ca56f8c3ecd8f3
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2022-10-06 19:43:40 +0300

    Frontend: Change products design

--- src/frontend/components/card.js
+++ src/frontend/components/card.js
@@ -2,14 +2,17 @@ import { Card, Col, Row, Text } from "@nextui-org/react";
 import { useState, useEffect } from "react";
 
 export default function CardLayer(props) {
+  const onPress = () => {
+    alert("pressed");
+  };
   return (
-    <Card variant="flat">
+    <Card isPressable isHoverable  variant="flat" css={{ w: "100%" }} onPress={onPress} >
       <Card.Body css={{ p: 0 }}>
         <Card.Image
           src={props.src}
           objectFit="cover"
           width="100%"
-          height="100%"
+          height={400}
           alt={props.title}
         />
       </Card.Body>
--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,37 +1,24 @@
 import Main from "../components/main";
 import CardLayer from "../components/card";
 import { Image, Grid } from "@nextui-org/react";
-import { useEffect, useState } from "react";
 
-import { useSession } from "next-auth/react"
-
-let count = 0
+let count = 0;
 export default function Home({ products }) {
-  const { data: session } = useSession()
   // useEffect(() => {
   //   console.log("useState")
   // }, []);
+
+  // const getProduct = () => {
+  //   count = count + 1;
+  //   return (
+  //     <CardLayer
+  //       src={products[count % Object.keys(products).length].src}
+  //       title={products[count % Object.keys(products).length].title}
+  //       price={products[count % Object.keys(products).length].price}
+  //     ></CardLayer>
+  //   );
+  // };
   
-  const getProduct = () => {
-    count = count + 1
-    return (
-      <CardLayer
-        src={products[count % Object.keys(products).length].src}
-        title={products[count % Object.keys(products).length].title}
-        price={products[count % Object.keys(products).length].price}
-      ></CardLayer>
-    );
-  };
-  const getUser = () => {
-    if (session) {
-      return (
-        <><p>Signed in as {session.user.email}</p></>
-      )
-    }
-    return (
-      <><p>Not signed in</p></>
-    )
-  }
   return (
     <Main>
       <Image
@@ -42,44 +29,50 @@ export default function Home({ products }) {
         weight="100%"
       />
       <header className="text-3xl font-bold pt-4 pl-8">Hot Products</header>
-      {getUser()}
       <Grid.Container gap={3} justify="center">
-        <Grid xs={12}>
-          <Grid.Container gap={3} justify="center">
-            <Grid xs={4}>{getProduct()}</Grid>
-            <Grid xs={4}>
-              <Grid.Container gap={1} justify="center">
-                <Grid xs={12}>{getProduct()}</Grid>
-                <Grid xs={12}>{getProduct()}</Grid>
-              </Grid.Container>
-            </Grid>
-            <Grid xs={4}>
-              <Grid.Container gap={1} justify="center">
-                <Grid xs={12}>{getProduct()}</Grid>
-                <Grid xs={12}>{getProduct()}</Grid>
-              </Grid.Container>
-            </Grid>
-          </Grid.Container>
-        </Grid>
-        <Grid xs={12}>
-          <Grid.Container gap={3} justify="center">
-            <Grid xs={4}>{getProduct()}</Grid>
-            <Grid xs={4}>{getProduct()}</Grid>
-            <Grid xs={4}>{getProduct()}</Grid>
-          </Grid.Container>
-        </Grid>
+        {products.map((p) => (
+          <Grid xs={12} sm={4}>
+            <CardLayer src={p.src} title={p.title} price={p.price}></CardLayer>
+          </Grid>
+        ))}
       </Grid.Container>
+      {/* <Grid.Container gap={3} justify="center">
+        <Grid xs={12} >
+          <Grid.Container gap={3} justify="center">
+            <Grid xs={4}>{getProduct()}</Grid>
+            <Grid xs={4}>
+              <Grid.Container gap={1} justify="center">
+                <Grid xs={12}>{getProduct()}</Grid>
+                <Grid xs={12}>{getProduct()}</Grid>
+              </Grid.Container>
+            </Grid>
+            <Grid xs={4}>
+              <Grid.Container gap={1} justify="center">
+                <Grid xs={12}>{getProduct()}</Grid>
+                <Grid xs={12}>{getProduct()}</Grid>
+              </Grid.Container>
+            </Grid>
+          </Grid.Container>
+        </Grid>
+        <Grid xs={12} >
+          <Grid.Container gap={3} justify="center">
+            <Grid xs={4}>{getProduct()}</Grid>
+            <Grid xs={4}>{getProduct()}</Grid>
+            <Grid xs={4}>{getProduct()}</Grid>
+          </Grid.Container>
+        </Grid>
+      </Grid.Container> */}
     </Main>
   );
 }
 
 export async function getStaticProps(context) {
-  const apiGateway = process.env.API_GATEWAY
-  const res = await fetch('http://'+ apiGateway +'/product/get')
-  const products = await res.json()
+  const apiGateway = process.env.API_GATEWAY;
+  const res = await fetch("http://" + apiGateway + "/product/get");
+  const products = await res.json();
   return {
     props: {
-      products: products.products
-    }
-  }
+      products: products.products,
+    },
+  };
 }
