5c27d1dedf50ecac6422346fbd09051f0c9c0d53
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2022-10-08 15:19:00 +0300

    Productservice: Add product endpoint

--- src/productservice/main.go
+++ src/productservice/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path"
 )
+
+type Product struct {
+	id    string `json:"id"`
+	src   string `json:"src"`
+	title string `json:"title"`
+	price string `json:"price"`
+}
 
 func main() {
 	fmt.Println("Product Service")
 
 	http.HandleFunc("/", root)
 	http.HandleFunc("/get", getProducts)
+	http.HandleFunc("/product/", getProduct)
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		log.Fatalf("There was an error listening port: 8080 %v", err)
@@ -31,8 +41,34 @@ func getProducts(w http.ResponseWriter, r *http.Request) {
 	w.Write(byteValue)
 }
 
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	productId := path.Base(r.URL.String())
+	for _, v := range parseProduct() {
+		id := fmt.Sprint(v["id"])
+		if id == productId {
+			fmt.Println(v)
+			b, _ := json.Marshal(v)
+			w.Write(b)
+		}
+	}
+}
+
 func root(w http.ResponseWriter, r *http.Request) {
 	// Forbidden to access root
 	fmt.Println("Hit to /")
 	w.WriteHeader(http.StatusForbidden)
+}
+
+func parseProduct() []map[string]interface{} {
+
+	p := []map[string]interface{}{}
+	byteValue, err := os.ReadFile("products.json")
+	if err != nil {
+		log.Fatalf("failed to open products json file: %v", err)
+	}
+	err = json.Unmarshal(byteValue, &p)
+	if err != nil {
+		log.Fatalf("There was an error unmarshalling %v", err)
+	}
+	return p
 }
